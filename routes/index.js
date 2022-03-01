94495194de54000406e3e32bd4dcf82e7ffe8a1c
RemixFX <remix.master@yandex.ru>
2022-03-01 16:02:02 +0300

    refactor: add validation file

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,14 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
 const usersRoute = require('./users');
 const moviesRoute = require('./movies');
+const { validationLogin, validationCreateUser } = require('../middlewares/validations');
 const { login, createUser, logout } = require('../controllers/users');
 const auth = require('../middlewares/auth');
 const NotFoundError = require('../errors/not-found-error');
 
-router.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }).messages({
-    'string.empty': 'Поле {#label} не может быть пустым',
-    'string.min': 'Поле {#label} должно быть минимум {#limit} символов',
-    'any.required': '{#label} - обязательное поле',
-    'string.email': 'Неверный формат почты',
-    'object.unknown': 'Переданы не разрешенные данные',
-  }),
-}), login);
+router.post('/signin', validationLogin, login);
 
-router.post('/signup', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-    name: Joi.string().required().min(2).max(30),
-  }).messages({
-    'string.empty': 'Поле {#label} не может быть пустым',
-    'string.min': 'Поле {#label} должно быть минимум {#limit} символов',
-    'string.max': 'Поле {#label} может быть максимум {#limit} символов',
-    'any.required': '{#label} - обязательное поле',
-    'string.email': 'Неверный формат почты',
-    'string.pattern.base': 'Некорректный адрес ссылки',
-  }),
-}), createUser);
+router.post('/signup', validationCreateUser, createUser);
 
 router.post('/signout', logout);
 
--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,9 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
+const { validationUpdateProfile } = require('../middlewares/validations');
 const { getMyProfile, updateProfile } = require('../controllers/users');
 
 router.get('/me', getMyProfile);
 
-router.patch('/me', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    email: Joi.string().required().email(),
-  }).messages({
-    'string.empty': 'Поле {#label} не может быть пустым',
-    'string.min': 'Поле {#label} должно быть минимум {#limit} символа',
-    'string.max': 'Поле {#label} может быть максимум {#limit} символов',
-    'any.required': '{#label} - обязательное поле',
-    'string.email': 'Неверный формат почты',
-    'object.unknown': 'Переданы не разрешенные данные',
-  }),
-}), updateProfile);
+router.patch('/me', validationUpdateProfile, updateProfile);
 
 module.exports = router;
